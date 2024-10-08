c48492ea05af8f9720d737ea4ffca5895a091321
Ryan Shephard (s5610934) <s5610934@bournemouth.ac.uk>
2024-10-08 17:29:48 +0100

    Display Message Changed

---
+++ my_first_project.py
@@ -0,0 +1,27 @@
+'''
+a = 11
+b = 5
+result = a < b 
+type_a = type(a)
+type_result = type(result)
+print(f"result = {result} and its data type is {type_a}")
+
+name = ("Ryan_Shephard")
+print(name[5])
+
+a = True
+
+
+
+x = "abdul"
+y = 21
+z = 10.5
+print(type(x))
+print(type(y))
+print(type(z))
+sum = y + z
+print("sum")
+'''
+
+my_var = 1
+
